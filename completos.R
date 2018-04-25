9433cd8e11b62d5dd4b3e94846f730915480edc8
José Guadalupe Zepeda Vizcarra <jose_guadalupe_viscarra@hotmail.com>
2018-04-25 08:34:58 -0500

    CASO 1
    Caso 1 Parte 2

---
+++ completos.R
@@ -0,0 +1,33 @@
+setwd("C:\Users\jose_\Downloads/specdata")
+directorio="C:\Users\jose_\Downloadss/specdata"
+
+completos <-function(directorio,id=1:332){
+  resultado<-c()
+  
+  for(i in id){
+    
+    if(nchar(i)==1){                
+      valor <- read.csv(paste("00",i,".csv", sep=""))
+    }
+    if(nchar(i)==2){                
+      valor<- read.csv(paste("0",i,".csv", sep=""))
+    }
+    if(nchar(i)==3){                 
+      valor <- read.csv(paste(i,".csv", sep=""))
+    }
+    
+    x <- data.matrix(valor)         
+    x[is.na(x)] <- 0               
+    suma <- 0
+    
+    for(k in 1:nrow(valor)){
+    if(x[k,2] > 0 ){
+    suma<- suma + 1   
+      }  
+    }
+    
+    resultado<- c(resultado, suma)    }
+  nobs<- resultado
+  data.frame(id,nobs)
+}
+completos(directorio, 1:20)
